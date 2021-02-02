e93a71dd37ae2662d0b180920e61a88b1b53f4cb
Alex Forrence <alex.forrence@gmail.com>
2021-02-02 13:12:26 -0500

    organize

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: path.resolve(__dirname) + "/build/psydapt.js",
+  entry: path.resolve(__dirname) + "/build/psydapt-es6.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "psydapt.min.js",
